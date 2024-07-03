ef9b1ec43da72245556c24edf6ca7b74491cdfb9
Mansi Shinde <shindemansi2804@gmail.com>
2024-07-03 16:18:10 +0530

    Basic changes in settings and array

--- src/Components/Investbond.js
+++ src/Components/Investbond.js
@@ -1,60 +1,83 @@
-import React from 'react'
-import "../Styles/Investbond.css"
+import React from 'react';
+import "../Styles/Investbond.css";
+import Slider from 'react-slick';
 
-let investbonds = [
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+
+const investbonds = [
     {
-        img:"https://assets.incredmoney.com/images/webp/corporate.webp",
-        title:"Corporate Bonds",
+        img: "https://assets.incredmoney.com/images/webp/corporate.webp",
+        title: "Corporate Bonds",
         text1: "0%-12% Fixed Returns",
         text2: "Investment Grade Rated Bonds",
         text3: "Secured & Listed",
     },
     {
-        img:"https://assets.incredmoney.com/images/webp/ipo.webp",
-        title:"NCD IPO",
+        img: "https://assets.incredmoney.com/images/webp/ipo.webp",
+        title: "NCD IPO",
         text1: "Invest Online up to 10L",
         text2: "No KYC Documents Required",
         text3: "Secured, Listed & Rated",
     },
     {
-        img:"https://assets.incredmoney.com/images/webp/mld.webp",
-        title:"Market Linked Debentures",
+        img: "https://assets.incredmoney.com/images/webp/mld.webp",
+        title: "Market Linked Debentures",
         text1: "100% Principal Protected",
         text2: "Nifty Linked",
         text3: "Secured, Listed & Rated",
     },
-]
+    {
+        img: "https://assets.incredmoney.com/images/webp/fd-products.webp",
+        title: "Fixed Deposits",
+        text1: "DICGC Insured",
+        text2: "Paperless Process",
+        text3: "Easy Withdrawal",
+    },
+];
 
 const Investbond = () => {
-  return (
-    <div className='investBonds'>
-        <div className='bondsTitle'>
-            <h1>Our Products</h1>
+    const settings = {
+        dots: false,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+    };
+
+    return (
+        <div className='investBonds'>
+            <div className='bondsTitle'>
+                <h1>Our Products</h1>
+            </div>
+            <div className='investsRow'>
+                <Slider {...settings}>
+                    {investbonds.map((investbond, i) => (
+                        <Investment key={i} invest={investbond} />
+                    ))}
+                </Slider>
+            </div>
         </div>
-        <div className='investsRow'>
-            {investbonds.map((investbond, i) => {
-                return <Investment key={i} invest= {investbond}/>
-            })}
-        </div>
-    </div>
-  )
+    );
 };
 
 const Investment = (props) => {
     return (
         <div className='entireBonds'>
-            <img src={props.invest.img}/>
+            <img src={props.invest.img} alt={props.invest.title} />
             <p>{props.invest.title}</p>
             <div className='lists'>
                 <ul>
-                <li>{props.invest.text1}</li>
-                <li>{props.invest.text2}</li>
-                <li>{props.invest.text3}</li>
+                    <li>{props.invest.text1}</li>
+                    <li>{props.invest.text2}</li>
+                    <li>{props.invest.text3}</li>
                 </ul>
                 <button className='details'>View Details</button>
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default Investbond
+export default Investbond;
