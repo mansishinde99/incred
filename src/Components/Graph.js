fde39d28333e98edfaeae2272d5aa162907f8b7a
Mansi Shinde <shindemansi2804@gmail.com>
2024-07-03 16:14:42 +0530

    added logo and img through api

--- src/Components/Graph.js
+++ src/Components/Graph.js
@@ -1,46 +1,46 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "../Styles/Graph.css";
 
-let logImages = [
-  {
-  img: "https://assets.incredmoney.com/images/webp/assetManagement.webp",
-  text: "₹ 1,185+ Crs",
-  subText: "Assets Under Management",
-  },
-  {
-    img: "https://assets.incredmoney.com/images/webp/avgReturns.webp",
-    text: "10%-12% XIRR",
-    subText: "Avg. Returns Earned",
-    },
-  {
-  img: "https://assets.incredmoney.com/images/webp/ontimePayment.webp",
-  text: "100%",
-  subText: "On Time Payments",
-  },
-  {
-  img: "https://assets.incredmoney.com/images/webp/investorCommunity.webp",
-  text: "1.3 Lakhs+",
-  subText: "Users",
-  },
-  
-]
-
 const Graph = () => {
+  const [logImages, setLogImages] = useState([]);
+
+  useEffect(() => {
+    const url = "https://api.incredmoney.com/content/stats";
+
+    const fetchData = async () => {
+      try {
+        const resp = await fetch(url);
+        const statsData = await resp.json();
+        console.log("Fetched Data:", statsData);
+        setLogImages(statsData.data);
+      } catch (error) {
+        console.log("error", error);
+      }
+    };
+    fetchData();
+  }, []);
+
   return (
     <div className="defaults">
-     <div className="date">
-      <p className="zero">0</p>
-      <p className="dtd">Defaults till Date</p>
-     </div>
-     <div className="payments">
-      {logImages.map((logImage) => (
-        <div className="logImage">
-          <img src={logImage.img}/>
-          <p className="text">{logImage.text}</p>
-          <p className="subtext">{logImage.subText}</p>
-        </div>
-      ))}
-     </div>
+
+      <div className="date">
+        <p className="zero">
+          <img src={logImages.imgUrl} />
+        </p>
+        <p className="dtd">{logImages.imgTitle}</p>
+      </div>
+
+      <div className="payments">
+        {logImages.stats?.map((logImage, i) => (
+          <div className="logImage" key={i}>
+            <div className="image">
+              <img src={logImage.imgUrl} alt="description" />
+            </div>
+            <p className="text">{logImage.text}</p>
+            <p className="subtext">{logImage.subtext}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
