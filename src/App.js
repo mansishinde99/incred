d2f55facc0400d8090226d656e726be7c87c3376
Mansi Shinde <shindemansi2804@gmail.com>
2024-07-12 11:36:09 +0530

    Basic Code Formating

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import React from "react";
 import "./App.css";
+
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import MAIN from "./Pages/MAIN";
+
+import Main from "./Pages/Main";
 import Login from "./NavPages/Login";
-
-
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route exact path="/" element={<MAIN />} />
+          <Route exact path="/" element={<Main />} />
           <Route exact path="/login" element={<Login />} />
           <Route exact path="/signup" element={<Login/>}/>
         </Routes>
